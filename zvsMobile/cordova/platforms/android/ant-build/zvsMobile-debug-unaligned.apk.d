849825236529847f21efe379e72d3fbfc18c5d1f
Aaron Drabeck <aaron-drabeck@leavitt.com>
2015-11-19 11:28:28 -0700

    Restores Native Back Button Functionality

--- zvsMobile/cordova/platforms/android/ant-build/zvsMobile-debug-unaligned.apk.d
+++ zvsMobile/cordova/platforms/android/ant-build/zvsMobile-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-M:\zvsMobile\zvsMobile\cordova\platforms\android\ant-build\zvsMobile-debug-unaligned.apk : \
-M:\zvsMobile\zvsMobile\cordova\platforms\android\ant-build\zvsMobile.ap_ \
-M:\zvsMobile\zvsMobile\cordova\platforms\android\ant-build\classes.dex \
-M:\zvsMobile\zvsMobile\cordova\platforms\android\CordovaLib\ant-build\classes.jar \
+M:\7-Sourcecode\zvsMobile\zvsMobile\cordova\platforms\android\ant-build\zvsMobile-debug-unaligned.apk : \
+M:\7-Sourcecode\zvsMobile\zvsMobile\cordova\platforms\android\ant-build\zvsMobile.ap_ \
+M:\7-Sourcecode\zvsMobile\zvsMobile\cordova\platforms\android\ant-build\classes.dex \
+M:\7-Sourcecode\zvsMobile\zvsMobile\cordova\platforms\android\CordovaLib\ant-build\classes.jar \
